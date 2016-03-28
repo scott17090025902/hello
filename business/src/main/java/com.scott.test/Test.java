cb53e536ef52edf08cbbd8811f2331c8b33610e4
gst <guosongtao@quicloud.cn>
2016-03-28 21:35:53 +0800

    SpringMVC Support

--- business/src/main/java/com.scott.test/Test.java
+++ business/src/main/java/com.scott.test/Test.java
@@ -1,10 +1,20 @@
 package com.scott.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 /**
  * Created by gst on 2016/3/28.
  */
+@Controller
+@RequestMapping("/test")
 public class Test {
-    public static void main(String[] args){
-        System.out.println("hello world!");
+    @ResponseBody
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String test() {
+        return "Hello World";
     }
 }
---
+++ business/src/main/webapp/jsp/index.jsp
@@ -0,0 +1,9 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: derek
+  Date: 15-1-24
+  Time: 下午2:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<p>你好</p>
