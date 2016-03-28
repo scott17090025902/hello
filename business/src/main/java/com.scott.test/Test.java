e45ec27eb54b625cac154ee6b9f9d41db6efec75
gst <guosongtao@quicloud.cn>
2016-03-28 19:08:31 +0800

    Initial commit

---
+++ business/src/main/java/com.scott.test/Test.java
@@ -0,0 +1,10 @@
+package com.scott.test;
+
+/**
+ * Created by gst on 2016/3/28.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
